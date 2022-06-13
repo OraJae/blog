b586782010adc72be9fa64c1d9018c2d751852bd
15629006909 <464060360@qq.com>
2022-06-13 17:47:12 +0800

    111

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import "vditor/dist/index.css"
 import VueRouter from 'vue-router'
 import router from './router'
 import plugins from './plugins'
